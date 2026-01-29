3b8e3b050c5a1157f149ee436d10a4d74b885387
许鑫 <xuxin@nl-ds.cn>
2026-01-29 11:25:18 +0800

    feat: add main entry point for react_json

--- demos/agent-framework/react_json.py
+++ demos/agent-framework/react_json.py
@@ -147,3 +147,18 @@ class ReActJSONAgent:
 
         print("已达到最大步数，流程终止。")
         return None
+
+
+if __name__ == "__main__":
+    from tools import search
+
+    llm = HelloAgentsLLM()
+    tool_executor = ToolExecutor()
+    search_desc = (
+        "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中"
+        "找不到的信息时，应使用此工具。"
+    )
+    tool_executor.registerTool("Search", search_desc, search)
+    agent = ReActJSONAgent(llm_client=llm, tool_executor=tool_executor)
+    question = "华为最新的手机是哪一款？它的主要卖点是什么？"
+    agent.run(question)
